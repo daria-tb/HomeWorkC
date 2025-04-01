3dcdf2a8798c14c75ae1a84b77e9d4ffe7e0c59b
Dasha <dascha.togan1@gmail.com>
2025-04-01 20:28:02 +0300

    BasicString.cpp

--- BasicString.cpp
+++ BasicString.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <iostream>
 
 template <class T> 
@@ -98,28 +97,47 @@ public:
 		len += addLen;
 	}
 
-	int find(const T* str) const
+	int find(const T* str) //str == "wor"
 	{
+		//this
+		
 		size_t strLen = std::strlen(str);
-		if (strLen > len) return -1;
+		
+		if (len < strLen) return -1;
 
-		for (size_t i = 0; i <= len - strLen; ++i)
+		
+		for (int i = 0; i < len - strLen; i++)
 		{
-			if (std::memcmp(data + i, str, strLen) == 0)
-				return static_cast<int>(i);
+			//if(std::memcmp(data + i, str, strLen) == 0))
+			int j = 0;
+			if (str[j] == data[i])
+			{
+				while (j < strLen)
+				{
+					if (str[j++] != data[i++])
+						break;
+				}
+				if (j == strLen)
+					return i - j;
+				else
+				{
+					i--; //may be
+					continue;
+				}
+			}
 		}
 		return -1;
 	}
-/* 	void append(const T* str)
-	{
-		size_t addLen = std::strlen(str);
-		resize(len + addLen + 1);
-		std::memcpy(data + len, str, addLen + 1);
-		len += addLen;
-	}
 
-	int find(const T* str)
-	{} */
+	friend BasicString<T> operator+(const BasicString<T>& left, const BasicString<T>& right)
+	{
+		BasicString<T> result;
+		result.resize(left.len + right.len + 1);
+		std::memcpy(result.data, left.data, left.len);
+		std::memcpy(result.data + left.len, right.data, right.len + 1);
+		result.len = left.len + right.len;
+		return result;
+	}
 
 private:
 	T* data;
@@ -139,3 +157,33 @@ private:
 		}
 	}
 };
+
+int main()
+{
+	BasicString<char> string_{ "Goodby, world!!!!" };
+
+	const char* str = "war";
+
+	int pos = string_.find(str);
+
+	std::cout << "index = " << pos << '\n';
+}
+/*
+data = "Hello world!!!" = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!', '!', '!', '\0'  "               "}
+                          [0]  [1]  [2]  [3]  [4]   [5]  [6]  [7]  [8]  [9]  [10] [11] [12] [13] 
+
+"Goodbye world!!!"
+
+str = "wor" return 6
+str = "war" return -1
+
+1) шукаємо перше входження першого символа з рядка str (str[0], нехай цей 0 буде позначатися індексом j) в рядок data;
+2) якщо не знайшли - повертаємо -1
+3) якщо знайшли (нехай його індекс буде дорівнювати i)
+	3.1) чи співпадає str[j + 1] з символом data[i + 1]?
+	3.2) якщо так, то j++, i++ та перейти до 3.1
+	3.3) якщо ні - то перейти до 4)
+4) шукаємо наступне входження першого символа з рядка str в рядок data
+5) повторюємо кроки 2-3
+
+*/
